59fd2608edcab856e67c9f42c97a7d2f0962ee2b
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2023-01-07 02:39:05 -0500

    enable new architecture

--- App.tsx
+++ App.tsx
@@ -8,54 +8,19 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
+import Analytics from 'appcenter-analytics';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -73,25 +38,20 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={styles.buttonStyle}>
+          <Button
+            title="Send Event"
+            accessibilityLabel="send event"
+            onPress={async () => {
+              await Analytics.setEnabled(true);
+
+              Analytics.trackEvent('Testing', {name: 'Tahmid', age: '32'})
+                .then(() => {
+                  console.log('event sent');
+                })
+                .catch(e => console.log(e));
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -114,6 +74,9 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  buttonStyle: {
+    margin: 20,
   },
 });
 
